b919530dc5296a5c4a3ccec56a29e94adffc37bf
adityamfu <akunpengganti.aditya@gmail.com>
2023-12-26 19:30:27 +0700

    fix video load by about route

--- lib/app/modules/about/controllers/about_controller.dart
+++ lib/app/modules/about/controllers/about_controller.dart
@@ -1,25 +1,20 @@
 import 'package:get/get.dart';
-import 'package:video_player/video_player.dart';
 
 class AboutController extends GetxController {
-   late VideoPlayerController videoController;
+  RxBool videoEnabled = false.obs;
 
-  final String videoUrl = "https://tvent.azurewebsites.net/assets/images/lv_0_20231108111358.mp4";
+  void enableVideo() {
+    videoEnabled.value = true;
+  }
 
   @override
   void onInit() {
     super.onInit();
-    videoController = VideoPlayerController.network(videoUrl);
-    videoController.initialize().then((_) {
-      videoController.setLooping(true); 
-      videoController.setVolume(0.0); 
-      update(); 
-    });
+    enableVideo();
   }
 
   @override
   void onClose() {
-    videoController.dispose();
     super.onClose();
   }
 
